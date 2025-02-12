6d4687f6213de73f9d14d6e0d8c5d057abab585b
hiroyce <114716994+hiroyce@users.noreply.github.com>
2025-02-11 20:29:49 -0500

    Update via-script.js - Delay Until All Scripts Are Loaded

--- via-script.js
+++ via-script.js
@@ -1,3 +1,4 @@
+<script type="text/javascript">
 // viaciclante.com overrides
 // dog
 // target text to add span/class 
@@ -99,26 +100,16 @@ observer.observe(document.body, { childList: true, subtree: true });
 
 
 // Product description overrides
-const intervalId = setInterval(() => {
+window.onload = () => {
   const productDescription = document.querySelector("#productDescription");
 
   if (productDescription) {
     const elements = productDescription.querySelectorAll("*");
-    let changesMade = false;
-
     elements.forEach((el) => {
-      if (el.hasAttribute("class") || el.hasAttribute("id") || el.hasAttribute("style")) {
-        el.removeAttribute("class");
-        el.removeAttribute("id");
-        el.removeAttribute("style");
-        changesMade = true;
-      }
+      el.removeAttribute("class");
+      el.removeAttribute("id");
+      el.removeAttribute("style");
     });
-
-    // If changes were made, stop the interval after ensuring everything is cleaned
-    if (changesMade) {
-      console.log("Attributes removed, stopping interval.");
-      clearInterval(intervalId);  // Stop checking once changes are made
-    }
   }
-}, 1000);  // Check every 1 second
+};
+</script>
