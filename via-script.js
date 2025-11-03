bc53f75afe074758980ba12abeb5ff18a2a6430a
hiroyce <114716994+hiroyce@users.noreply.github.com>
2025-11-03 14:40:20 -0500

    Change paragraph selectors to span selectors
    Lightspeed updating their semantic html again

--- via-script.js
+++ via-script.js
@@ -14,13 +14,13 @@ document.querySelectorAll("#tile-customer-review-BpeHf4 .ins-tile__title").forEa
   }  
 });
 
-document.querySelectorAll("#tile-category-products-sRPA5C .ins-tile__title p").forEach((el) => {
+document.querySelectorAll("#tile-category-products-sRPA5C .ins-tile__title span").forEach((el) => {
   if (el.innerHTML.includes("Sale")) {
     el.innerHTML = el.innerHTML.replace(/Sale/g, '<span class="collection">Sale</span>');
   }
 });
 
-document.querySelectorAll("#tile-category-collection-QtcTDn .ins-tile__title p").forEach((el) => {
+document.querySelectorAll("#tile-category-collection-QtcTDn .ins-tile__title span").forEach((el) => {
   if (el.innerHTML.includes("Cool Weather Riding")) {
     el.innerHTML = el.innerHTML.replace(/Cool Weather Riding/g, '<span class="collection">Cool Weather Riding</span>');
   }
